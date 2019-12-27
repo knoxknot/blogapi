d8b9b3928389347a7d2300d4baccab300ea920ac
Samuel Nwoye <nwoyesamuelc@gmail.com>
2019-12-27 17:55:14 +0100

    defined routes and implemented a get endpoint for all article retrival

--- main.go
+++ main.go
@@ -21,7 +21,21 @@ var articles = []article{
 	article{ID: "3",Title: "Go Mastery", Content: "If you love simplicity then you should master Go"},
 }
 
+// implement a retrival of all existing articles
+func getAllArticles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(articles)
+	w.WriteHeader(http.StatusOK)
+}
+
 // program entry point
 func main() {
-
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/articles", getAllArticles).Methods("GET")
+	api.HandleFunc("/articles/{id}", getAnArticle).Methods("GET")
+	api.HandleFunc("/articles", createAnArticle).Methods("POST")
+	api.HandleFunc("/articles/{id}", updateAnArticle).Methods("PUT")
+	api.HandleFunc("/articles/{id}", deleteAnArticle).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080",r))
 }
