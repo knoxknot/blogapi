d44915d6d43050a4e3bcc419d7f881382b74b754
Samuel Nwoye <nwoyesamuelc@gmail.com>
2019-12-27 17:50:55 +0100

    updated main.go with a mock data of articles

--- main.go
+++ main.go
@@ -14,6 +14,13 @@ type article struct {
 	Content string `json:"Content"`
 }
 
+// A slice mock data of all articles described by the article struct type
+var articles = []article{
+	article{ID: "1",Title: "Introduction to Go", Content: "This is a brief about Go and its awesomeness"},
+	article{ID: "2",Title: "The simplicity of Go", Content: "There is so much Awesomeness in Go"},
+	article{ID: "3",Title: "Go Mastery", Content: "If you love simplicity then you should master Go"},
+}
+
 // program entry point
 func main() {
 
